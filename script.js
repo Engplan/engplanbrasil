601ca3e9f97b559d5c759522e90a204cb169ef31
Engplan <engplanbrasil@gmail.com>
2025-11-17 22:01:17 -0300

    Atualização completa: banner 3D, paralaxe, celular flutuante e camadas

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Seleciona todos os elementos com classe .parallax
-const parallaxItems = document.querySelectorAll(".parallax");
-
-window.addEventListener("scroll", () => {
-    const scrollTop = window.pageYOffset;
-
-    parallaxItems.forEach(item => {
-        const speed = item.dataset.speed;
-        item.style.transform = `translateY(${scrollTop * speed * 0.01}px)`;
-    });
-});
+// futuro script
