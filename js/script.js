f0118e123b3c78f3edb708508e788572c71fb1bb
Engplan <engplanbrasil@gmail.com>
2026-02-11 01:19:57 -0300

    Padrão Absoluto Engplan Brasil - v1.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// futuro script
+// Inicializar Animações
+AOS.init();
+
+// Funções dos Modais
+function openModal(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+// Fechar modal ao clicar fora dele
+window.onclick = function(event) {
+    if (event.target.className === "modal") {
+        event.target.style.display = "none";
+    }
+}
